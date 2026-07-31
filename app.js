efac96df07d21c2b49d3a5758499f87b8c7f5a5f
molamike <120581758+molamikedevs@users.noreply.github.com>
2026-07-31 13:52:57 +0400

    Add and display friends list

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-const name = prompt('What is your name?');
-const greet = alert(`Hello 👋 ${name}`);
+// const name = prompt('What is your name?');
+// const greet = alert(`Hello 👋 ${name}`);
+
+const friends = ['Alhaji', 'Ibrahim', 'Mohamed', 'Chernor', 'Osman'];
+
+for (let i = 0; i < friends.length; i++) {
+  console.log(friends[i]);
+}
